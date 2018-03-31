5d253824cf82cb15574e5065e23c066ad6c3e558
Yaroslav Klymko <t3hnar@gmail.com>
2018-03-31 20:17:12 +0200

    PL-6796 upgrade "sequentially" dependency in order to fix exception handling issue

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object Dependencies {
   lazy val ScalaTest = "org.scalatest" %% "scalatest" % "3.0.5" % Test
-  lazy val KafkaClients = "org.apache.kafka" % "kafka-clients" % "1.0.0"
+  lazy val KafkaClients = "org.apache.kafka" % "kafka-clients" % "1.0.1"
   lazy val ExecutorTools = "com.evolutiongaming" %% "executor-tools" % "1.0.0"
-  lazy val Sequentially = "com.evolutiongaming" %% "sequentially" % "1.0.3"
-  lazy val Nel = "com.evolutiongaming" %% "nel" % "1.0"
+  lazy val Sequentially = "com.evolutiongaming" %% "sequentially" % "1.0.7"
+  lazy val Nel = "com.evolutiongaming" %% "nel" % "1.2"
   lazy val ConfigTools = "com.evolutiongaming" %% "config-tools" % "1.0.1"
   lazy val MetricTools = "com.evolutiongaming" %% "metric-tools" % "1.1"
-  lazy val SafeActor = "com.evolutiongaming" %% "safe-actor" % "1.4"
+  lazy val SafeActor = "com.evolutiongaming" %% "safe-actor" % "1.6"
   lazy val Prometheus = "io.prometheus" % "simpleclient" % "0.0.26"
 }

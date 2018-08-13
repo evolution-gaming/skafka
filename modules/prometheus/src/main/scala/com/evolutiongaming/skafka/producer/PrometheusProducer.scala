eb3bb0f5cb9f978c5364ae53060c3719516c32a6
Yaroslav Klymko <t3hnar@gmail.com>
2018-08-13 22:02:34 +0200

    fix #1

--- modules/prometheus/src/main/scala/com/evolutiongaming/skafka/producer/PrometheusProducer.scala
+++ modules/prometheus/src/main/scala/com/evolutiongaming/skafka/producer/PrometheusProducer.scala
@@ -10,14 +10,18 @@ import scala.util.{Failure, Success}
 
 object PrometheusProducer {
 
-  def apply(producer: Producer, registry: CollectorRegistry, prefix: String = "skafka_producer"): Producer = {
+  def apply(
+    producer: Producer,
+    registry: CollectorRegistry,
+    prefix: String = "skafka_producer",
+    label: String = "skafka"): Producer = {
 
     implicit val ec = CurrentThreadExecutionContext
 
     val latencySummary = Summary.build()
       .name(s"${ prefix }_latency")
       .help("Latency in seconds")
-      .labelNames("topic")
+      .labelNames("producer", "topic")
       .quantile(0.9, 0.01)
       .quantile(0.99, 0.001)
       .register(registry)
@@ -25,13 +29,13 @@ object PrometheusProducer {
     val bytesSummary = Summary.build()
       .name(s"${ prefix }_bytes")
       .help("Message size in bytes")
-      .labelNames("topic")
+      .labelNames("producer", "topic")
       .register(registry)
 
     val counter = Counter.build()
       .name(s"${ prefix }_result")
       .help("Result: success or failure")
-      .labelNames("topic", "result")
+      .labelNames("producer", "topic", "result")
       .register(registry)
 
     new Producer {
@@ -45,19 +49,20 @@ object PrometheusProducer {
           val topicLabel = record.topic.replace(".", "_")
           val duration = (Platform.currentTime - start).toDouble / 1000
           latencySummary
-            .labels(topicLabel)
+            .labels(label, topicLabel)
             .observe(duration)
 
           val resultLabel = result match {
             case Success(metadata) =>
               bytesSummary
-                .labels(topicLabel)
+                .labels(label, topicLabel)
                 .observe(metadata.serializedValueSize.toDouble)
               "success"
-            case Failure(_)        => "failure"
+            case Failure(_)        =>
+              "failure"
           }
           counter
-            .labels(topicLabel, resultLabel)
+            .labels(label, topicLabel, resultLabel)
             .inc()
         }
         result

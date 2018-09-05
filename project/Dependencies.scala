02d6bbe5f547a7768f90d81257e9ced920921ee6
Yaroslav Klymko <t3hnar@gmail.com>
2018-09-05 21:49:02 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val `executor-tools` = "com.evolutiongaming" %% "executor-tools" % "1.0.1"
   lazy val sequentially = "com.evolutiongaming" %% "sequentially" % "1.0.12"
   lazy val nel = "com.evolutiongaming" %% "nel" % "1.3.1"
-  lazy val `config-tools` = "com.evolutiongaming" %% "config-tools" % "1.0.2"
+  lazy val `config-tools` = "com.evolutiongaming" %% "config-tools" % "1.0.3"
   lazy val `metric-tools` = "com.evolutiongaming" %% "metric-tools" % "1.1"
   lazy val `safe-actor` = "com.evolutiongaming" %% "safe-actor" % "1.7"
   lazy val Prometheus = "io.prometheus" % "simpleclient" % "0.5.0"
@@ -15,7 +15,7 @@ object Dependencies {
   lazy val `kafka-launcher` = "com.evolutiongaming" %% "kafka-launcher" % "0.0.1" % Test
 
   object Akka {
-    private val version = "2.5.15"
+    private val version = "2.5.16"
     lazy val testkit = "com.typesafe.akka" %% "akka-testkit" % version % Test
     lazy val slf4j = "com.typesafe.akka" %% "akka-slf4j" % version % Test
   }

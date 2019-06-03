ec46d8e07e4421a055fe40596b851c0e2706cb8a
asavelyev <asavelyev@evolutiongaming.com>
2019-06-03 13:41:12 +0200

    Further refine type class usage in `LoggingProducer`

--- modules/logging/src/main/scala/com/evolutiongaming/skafka/producer/LoggingProducer.scala
+++ modules/logging/src/main/scala/com/evolutiongaming/skafka/producer/LoggingProducer.scala
@@ -1,18 +1,14 @@
 package com.evolutiongaming.skafka.producer
 
 import cats.implicits._
-import cats.{FlatMap, MonadError}
+import cats.{ApplicativeError, FlatMap}
 import com.evolutiongaming.catshelper.Log
 import com.evolutiongaming.skafka.{OffsetAndMetadata, ToBytes, Topic, TopicPartition}
 
 import scala.concurrent.duration.FiniteDuration
 
 object LoggingProducer {
-  type MonadThrowable[F[_]] = MonadError[F, Throwable]
-
-  object MonadThrowable {
-    def apply[F[_]](implicit F: MonadThrowable[F]): MonadThrowable[F] = F
-  }
+  type MonadThrowable[F[_]] = ApplicativeError[F, Throwable]
 
   def apply[F[_] : MonadThrowable : FlatMap](producer: Producer[F], log: Log[F]): Producer[F] = {
 
@@ -33,10 +29,11 @@ object LoggingProducer {
       override def send[K: ToBytes, V: ToBytes](record: ProducerRecord[K, V]): F[RecordMetadata] = {
         producer.send(record).attempt.flatMap {
           case Right(metadata) =>
-            log.debug(s"sent $record, metadata: $metadata").as(metadata)
+            log.debug(s"sent $record, metadata: $metadata")
+              .flatMap(_ => metadata.pure[F])
           case Left(failure)   =>
-            log.error(s"failed to send record $record: $failure") *>
-              MonadThrowable[F].raiseError[RecordMetadata](failure)
+            log.error(s"failed to send record $record: $failure")
+              .flatMap(_ => failure.raiseError[F, RecordMetadata])
         }
       }
 

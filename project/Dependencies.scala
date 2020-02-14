1bab065529c865bf7002d820ba12e579bbd113f5
Scala Steward <me@scala-steward.org>
2020-02-14 14:16:54 +0100

    Update smetrics to 0.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   val `scala-java8-compat` = "org.scala-lang.modules" %% "scala-java8-compat"      % "0.9.0"
   val `collection-compat`  = "org.scala-lang.modules" %% "scala-collection-compat" % "2.1.3"
   val scalatest            = "org.scalatest"          %% "scalatest"               % "3.1.0"
-  val smetrics             = "com.evolutiongaming"    %% "smetrics"                % "0.0.8"
+  val smetrics             = "com.evolutiongaming"    %% "smetrics"                % "0.1.0"
   val `kind-projector`     = "org.typelevel"           % "kind-projector"          % "0.10.3"
 
   object Kafka {

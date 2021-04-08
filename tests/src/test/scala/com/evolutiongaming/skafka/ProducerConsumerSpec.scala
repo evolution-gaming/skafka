a704d40d6b0f2cc02a202d09537ef315f18c7e00
Nikita Matveenko <nikitapecasa@gmail.com>
2021-04-08 13:53:07 +0200

    wip: add test to make sure that context shift has no effect on interaction with kafka java consumer from within poll method

--- tests/src/test/scala/com/evolutiongaming/skafka/ProducerConsumerSpec.scala
+++ tests/src/test/scala/com/evolutiongaming/skafka/ProducerConsumerSpec.scala
@@ -169,6 +169,19 @@ class ProducerConsumerSpec extends AnyFunSuite with BeforeAndAfterAll with Match
 
         def onPartitionsAssigned(partitions: Nes[TopicPartition]) = {
           for {
+            _ <- lift(IO.shift)
+            _ <- lift(
+              IO.shift *>
+                IO.delay(()) *>
+                IO.shift *>
+                IO.delay(())
+            )
+            // with IO.shift attempts we make sure that
+            // kafka java consumer.position method would be called from the same thread as consumer.poll
+            // otherwise kafka java consumer throws
+            // ConcurrentModificationException("KafkaConsumer is not safe for multi-threaded access")
+            // which in result would terminate current flatMap chain
+            // and fail the test as positions (Set[Offset]) would be empty
             position <- position(partitions.head) // TODO: allow consumer error handling in user land
             _ <- lift(positions.update(_.+(position)))
             _ <- lift(rebalanceCounter.update(_ + 1))

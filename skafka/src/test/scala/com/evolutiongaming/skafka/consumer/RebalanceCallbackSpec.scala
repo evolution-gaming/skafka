b79c094ecbaec580f7f0311d822c23aad4fa1aba
Nikita Matveenko <nikitapecasa@gmail.com>
2021-04-09 14:02:44 +0200

    wip: add flatMap tests in RebalanceCallbackSpec

--- skafka/src/test/scala/com/evolutiongaming/skafka/consumer/RebalanceCallbackSpec.scala
+++ skafka/src/test/scala/com/evolutiongaming/skafka/consumer/RebalanceCallbackSpec.scala
@@ -7,6 +7,7 @@ import java.util.regex.Pattern
 import java.util.{Collection => CollectionJ, List => ListJ, Map => MapJ, Set => SetJ}
 
 import cats.data.{NonEmptyMap => Nem, NonEmptySet => Nes}
+import cats.effect.IO
 import com.evolutiongaming.skafka.consumer.RebalanceCallback._
 import com.evolutiongaming.skafka.consumer.RebalanceCallbackSpec._
 import com.evolutiongaming.skafka.{Offset, Partition, TopicPartition}
@@ -20,10 +21,12 @@ import org.apache.kafka.clients.consumer.{
 import org.apache.kafka.common.{Metric, MetricName, PartitionInfo, TopicPartition => TopicPartitionJ}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
+import com.evolutiongaming.skafka.IOSuite._
 
 import scala.concurrent.duration._
 import scala.jdk.CollectionConverters._
-import scala.util.Try
+import scala.util.{Failure, Try}
+import scala.util.control.NoStackTrace
 
 class RebalanceCallbackSpec extends AnyFreeSpec with Matchers {
 
@@ -42,35 +45,6 @@ class RebalanceCallbackSpec extends AnyFreeSpec with Matchers {
     }
 
     "consumer related methods delegating the call correctly" - {
-      val partitions: JavaScala[SetJ[TopicPartitionJ], Nes[TopicPartition]] = JavaScala(
-        Set(
-          new TopicPartitionJ("topic", 3),
-          new TopicPartitionJ("topicc", 42)
-        ).asJava,
-        Nes.of(
-          TopicPartition("topic", Partition.unsafe(3)),
-          TopicPartition("topicc", Partition.unsafe(42))
-        )
-      )
-
-      val offsetsMap: JavaScala[MapJ[TopicPartitionJ, LongJ], Nem[TopicPartition, Offset]] = JavaScala(
-        Set(
-          new TopicPartitionJ("topic", 3)   -> LongJ.valueOf(39L),
-          new TopicPartitionJ("topicc", 42) -> LongJ.valueOf(71L)
-        ).toMap.asJava,
-        Nes
-          .of(
-            TopicPartition("topic", Partition.unsafe(3))   -> Offset.unsafe(39),
-            TopicPartition("topicc", Partition.unsafe(42)) -> Offset.unsafe(71)
-          )
-          .toNonEmptyList
-          .toNem
-      )
-
-      val timeouts: JavaScala[DurationJ, FiniteDuration] = JavaScala(
-        DurationJ.ofSeconds(7),
-        7.seconds
-      )
 
       "assignment" in {
         val expected = partitions.s
@@ -108,15 +82,84 @@ class RebalanceCallbackSpec extends AnyFreeSpec with Matchers {
 
     }
 
+    "composability" - {
+      "flatMap" in {
+        val expected               = partitions.s
+        var a: Set[TopicPartition] = Set.empty
+        var b: String              = "unchanged"
+        var c: String              = "unchanged"
+
+        val consumer = new ExplodingConsumer {
+          override def assignment(): SetJ[TopicPartitionJ] = partitions.j
+
+          override def paused(): SetJ[TopicPartitionJ] = throw TestError2
+        }
+
+        val rcOk = for {
+          _      <- noOp[IO]
+          result <- assignment
+          _       = lift(IO.raiseError[Unit](TestError)) // should have no effect
+          _       = paused // throws TestError2 but should have no effect
+          _ <- lift(IO.delay {
+            a = result
+          })
+        } yield ()
+
+        val rcError1 = for {
+          _ <- lift(IO.delay {
+            c = "rcError1"
+          })
+          _ <- lift(IO.raiseError[Unit](TestError)) // should fail the execution
+          _ <- paused // paused throws TestError2, should not overwrite first error from lift
+          _ <- lift(IO.delay {
+            b = "this change should not happen"
+          })
+        } yield ()
+
+        val rcError2 = for {
+          _ <- lift(IO.delay {
+            c = "rcError2"
+          })
+          _ <- paused // throws TestError2
+          _ <- lift(
+            IO.raiseError[Unit](TestError)
+          ) // execution is failed already, should not overwrite first error from paused
+          _ <- lift(IO.delay {
+            b = "this change should not happen 2"
+          })
+        } yield ()
+
+        val ok = RebalanceCallback.run(rcOk, consumer)
+        ok mustBe Try(())
+        a mustBe expected
+        c mustBe "unchanged"
+
+        val Failure(error) = RebalanceCallback.run(rcError1, consumer)
+        error mustBe TestError
+        b mustBe "unchanged"
+        c mustBe "rcError1"
+
+        val Failure(error2) = RebalanceCallback.run(rcError2, consumer)
+        error2 mustBe TestError2
+        b mustBe "unchanged"
+        c mustBe "rcError2"
+      }
+
+    }
   }
 }
 
 object RebalanceCallbackSpec {
+
   def tryRun[A](rc: RebalanceCallback[Try, A], consumer: ConsumerJ[_, _]): Try[A] = {
     RebalanceCallback.run[Try, A](rc, consumer)
   }
 
   final case class JavaScala[J, S](j: J, s: S)
+
+  case object TestError extends NoStackTrace
+
+  case object TestError2 extends NoStackTrace
 
   // - it is intentional to have all methods as `???` (throws NotImplementedError)
   // - it is used to verify the only expected interaction in corresponding tests
@@ -228,4 +271,34 @@ object RebalanceCallbackSpec {
 
     def wakeup(): Unit = ???
   }
+
+  val partitions: JavaScala[SetJ[TopicPartitionJ], Nes[TopicPartition]] = JavaScala(
+    Set(
+      new TopicPartitionJ("topic", 3),
+      new TopicPartitionJ("topicc", 42)
+    ).asJava,
+    Nes.of(
+      TopicPartition("topic", Partition.unsafe(3)),
+      TopicPartition("topicc", Partition.unsafe(42))
+    )
+  )
+
+  val offsetsMap: JavaScala[MapJ[TopicPartitionJ, LongJ], Nem[TopicPartition, Offset]] = JavaScala(
+    Set(
+      new TopicPartitionJ("topic", 3)   -> LongJ.valueOf(39L),
+      new TopicPartitionJ("topicc", 42) -> LongJ.valueOf(71L)
+    ).toMap.asJava,
+    Nes
+      .of(
+        TopicPartition("topic", Partition.unsafe(3))   -> Offset.unsafe(39),
+        TopicPartition("topicc", Partition.unsafe(42)) -> Offset.unsafe(71)
+      )
+      .toNonEmptyList
+      .toNem
+  )
+
+  val timeouts: JavaScala[DurationJ, FiniteDuration] = JavaScala(
+    DurationJ.ofSeconds(7),
+    7.seconds
+  )
 }

a3aadb42427f26356229ed5d2e1a709234c822af
Scala Steward <me@scala-steward.org>
2021-12-06 02:18:10 +0100

    Update play-json-jsoniter to 0.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val `future-helper`      = "com.evolutiongaming"    %% "future-helper"           % "1.0.6"
   val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"             % "2.7.0"
   val `kafka-launcher`     = "com.evolutiongaming"    %% "kafka-launcher"          % "0.0.11"
-  val `play-json-jsoniter` = "com.evolutiongaming"    %% "play-json-jsoniter"      % "0.9.3"
+  val `play-json-jsoniter` = "com.evolutiongaming"    %% "play-json-jsoniter"      % "0.10.0"
   val `scala-java8-compat` = "org.scala-lang.modules" %% "scala-java8-compat"      % "1.0.0"
   val `collection-compat`  = "org.scala-lang.modules" %% "scala-collection-compat" % "2.5.0"
   val scalatest            = "org.scalatest"          %% "scalatest"               % "3.2.10"
@@ -17,30 +17,30 @@ object Dependencies {
 
   object Kafka {
     private val version = "2.7.2"
-    val kafka   = "org.apache.kafka" %% "kafka"         % version
-    val clients = "org.apache.kafka" %  "kafka-clients" % version
+    val kafka           = "org.apache.kafka" %% "kafka"         % version
+    val clients         = "org.apache.kafka"  % "kafka-clients" % version
   }
 
   object Logback {
     private val version = "1.2.7"
-    val core    = "ch.qos.logback" % "logback-core"    % version
-    val classic = "ch.qos.logback" % "logback-classic" % version
+    val core            = "ch.qos.logback" % "logback-core"    % version
+    val classic         = "ch.qos.logback" % "logback-classic" % version
   }
 
   object Slf4j {
-    private val version = "1.7.32"
+    private val version    = "1.7.32"
     val api                = "org.slf4j" % "slf4j-api"        % version
     val `log4j-over-slf4j` = "org.slf4j" % "log4j-over-slf4j" % version
   }
 
   object Cats {
     private val version = "2.6.1"
-    val core = "org.typelevel" %% "cats-core"   % version
-    val laws = "org.typelevel" %% "cats-laws"   % version
+    val core            = "org.typelevel" %% "cats-core" % version
+    val laws            = "org.typelevel" %% "cats-laws" % version
   }
 
   object CatsEffect {
     private val version = "2.5.4"
-    val effect = "org.typelevel" %% "cats-effect" % version
+    val effect          = "org.typelevel" %% "cats-effect" % version
   }
 }

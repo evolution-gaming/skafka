9072ad609c820f147d5473d04e4294da2e3f1c5e
Kirill Shelopugin <kshelopugin@gmail.com>
2023-08-24 16:11:06 +0200

    Add ProducerOf.apply2/ConsumerOf.apply2 for binary compatibility (#376)

--- skafka/src/main/scala/com/evolutiongaming/skafka/consumer/ConsumerOf.scala
+++ skafka/src/main/scala/com/evolutiongaming/skafka/consumer/ConsumerOf.scala
@@ -38,6 +38,16 @@ object ConsumerOf {
     }
   }
 
+  /** The sole purpose of this method is to support binary compatibility with an intermediate
+   *  version (namely, 15.2.0) which had `apply1` method using `MeasureDuration` from `smetrics`
+   *  and `apply2` using `MeasureDuration` from `cats-helper`.
+   *  This should not be used and should be removed in a reasonable amount of time.
+   */
+  @deprecated("Use `apply1`", since = "16.0.3")
+  def apply2[F[_] : Async : ToTry : ToFuture : MeasureDuration](
+    metrics: Option[ConsumerMetrics[F]] = None
+  ): ConsumerOf[F] = apply1(metrics)
+
   implicit class ConsumerOfOps[F[_]](val self: ConsumerOf[F]) extends AnyVal {
 
     def mapK[G[_]](

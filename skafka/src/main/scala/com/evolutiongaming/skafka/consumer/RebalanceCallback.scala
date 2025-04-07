8ff1201bc0ce73d22a274f96f1a58ddaa5bd6366
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2025-04-07 19:55:19 +0300

    expose `currentLag` in `RebalanceCallback` and in `RebalanceConsumer`

--- skafka/src/main/scala/com/evolutiongaming/skafka/consumer/RebalanceCallback.scala
+++ skafka/src/main/scala/com/evolutiongaming/skafka/consumer/RebalanceCallback.scala
@@ -356,6 +356,9 @@ sealed trait RebalanceCallbackApi[F[_]] {
   final def subscription: RebalanceCallback[F, Set[Topic]] =
     WithConsumer(_.subscription())
 
+  final def currentLag(partition: TopicPartition): RebalanceCallback[F, Option[Long]] =
+    WithConsumer(_.currentLag(partition))
+
 }
 
 private[consumer] trait RebalanceCallbackLowPrioInstances {
